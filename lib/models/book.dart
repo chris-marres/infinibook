17231ab297af9c0e71ccdfdf7d4484edddbcf947
timtsapras23 <themistsapras7@hotmail.com>
2022-03-03 01:13:20 +0200

    Added data blocks books

---
+++ lib/models/book.dart
@@ -0,0 +1,17 @@
+class Book {
+  final String imagePath;
+  final String name;
+  final String author;
+  final String language;
+  final List<String> genres;
+  final int pages;
+
+  Book({
+    required this.imagePath,
+    required this.name,
+    required this.author,
+    required this.language,
+    required this.genres,
+    required this.pages,
+  });
+}
--- lib/models/dummy_data.dart
+++ lib/models/dummy_data.dart
@@ -1,7 +1,22 @@
+import 'package:infinibook_flutter/models/book.dart';
 import 'package:infinibook_flutter/models/user.dart';
 
 class DummyData {
   static List<User> users = [
+    User(
+        imagePath: 'assets/images/chris_500.jpeg',
+        name: 'Chris Marres',
+        email: 'chris.marres@gmail.com',
+        books: 4,
+        followers: 4,
+        following: 2),
+    User(
+        imagePath: 'assets/Faces/Themis Tsaprazlis.jpg',
+        name: 'Themis Tsaprazlis',
+        email: 'tim.tsapras@gmail.com',
+        books: 3,
+        followers: 5,
+        following: 3),
     User(
         imagePath: 'assets/Faces/Amanda Terry.jpg',
         name: 'Amanda Terry',
@@ -15,13 +30,6 @@ class DummyData {
         email: 'chriskross@gmail.com',
         books: 1,
         followers: 4,
-        following: 3),
-    User(
-        imagePath: 'assets/Faces/Themis Tsaprazlis.jpg',
-        name: 'Themis Tsaprazlis',
-        email: 'tim.tsapras@gmail.com',
-        books: 3,
-        followers: 5,
         following: 3),
     User(
         imagePath: 'assets/Faces/George Pagonis.jpg',
@@ -65,5 +73,137 @@ class DummyData {
         books: 5,
         followers: 2,
         following: 2),
+  ];
+
+  static List<Book> books = [
+    Book(
+        imagePath: 'assets/Books/A Game of Thrones_2.jpeg',
+        name: 'A Game of Thrones',
+        author: 'George R.R. Martin',
+        language: 'English',
+        genres: [
+          'Novel',
+          'High fantasy',
+          'Fantasy Fiction',
+          'Political fiction'
+        ],
+        pages: 694),
+    Book(
+        imagePath: 'assets/Books/A Game of Thrones.jpeg',
+        name: 'A Game of Thrones',
+        author: 'George R.R. Martin',
+        language: 'English',
+        genres: [
+          'Novel',
+          'High fantasy',
+          'Fantasy Fiction',
+          'Political fiction'
+        ],
+        pages: 694),
+    Book(
+        imagePath: 'assets/Books/Dune_4.jpg',
+        name: 'Dune',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 869),
+    Book(
+        imagePath: 'assets/Books/Dune_6.jpg',
+        name: 'Dune',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 592),
+    Book(
+        imagePath: 'assets/Books/Dune_3.jpg',
+        name: 'Dune Messiah',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 304),
+    Book(
+        imagePath: 'assets/Books/Dune_3.jpg',
+        name: 'Dune Messiah',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 304),
+    Book(
+        imagePath: 'assets/Books/Dune_5.jpg',
+        name: 'Dune: Deluxe Hardcover',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 688),
+    Book(
+        imagePath: 'assets/Books/Dune_1.jpg',
+        name: 'Dune (Barnes & Noble Collectible Editions)',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 576),
+    Book(
+        imagePath: 'assets/Books/Dune_8.jpg',
+        name: 'Dune',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 528),
+    Book(
+        imagePath: 'assets/Books/Dune_2.jpg',
+        name: 'Dune',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 528),
+    Book(
+        imagePath: 'assets/Books/Dune_8.jpg',
+        name: 'Dune',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 528),
+    Book(
+        imagePath: 'assets/Books/Dune_9.jpg',
+        name: 'Dune',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 528),
+    Book(
+        imagePath: 'assets/Books/Dune_10.jpg',
+        name: 'Dune',
+        author: 'Frank Herbert',
+        language: 'English',
+        genres: ['Science fiction'],
+        pages: 528),
+    Book(
+        imagePath: 'assets/Books/Lord of the Rings Fellowship of the Ring.jpeg',
+        name: 'Lord of the Rings:The Fellowship of the Ring',
+        author: 'J.R.R. Tolkien',
+        language: 'English',
+        genres: ['Fantasy'],
+        pages: 531),
+    Book(
+        imagePath: 'assets/Books/Fifty Shades of Grey.jpg',
+        name: 'Fifty Shades of Grey',
+        author: 'E.L. James',
+        language: 'English',
+        genres: ['Novel', 'Romance novel', 'Fiction', 'Erotic romance novel'],
+        pages: 514),
+    Book(
+        imagePath: 'assets/Books/Five Feet Apart.jpg',
+        name: 'Five Feet Apart',
+        author: 'Rachael Lippincott',
+        language: 'English',
+        genres: ['Adult Fiction'],
+        pages: 288),
+    Book(
+        imagePath: 'assets/Books/Fortress of Blood.jpg',
+        name: 'Fortress of Blood',
+        author: 'L. D. Goffigan',
+        language: 'English',
+        genres: ['Adult Fiction'],
+        pages: 288),
   ];
 }
