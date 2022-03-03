3011847961bafebed6adf383e8642405a25fdd0c
timtsapras23 <themistsapras7@hotmail.com>
2022-03-03 17:34:04 +0200

    Search Page v1

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:infinibook_flutter/models/dummy_data.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -11,9 +12,53 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search'),
-      ),
-    );
+        appBar: AppBar(title: const Text('Search'), actions: <Widget>[
+      IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            showSearch(context: context, delegate: DataSearch());
+          })
+    ]));
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+        onPressed: () {
+          close(context, "");
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? DummyData.books
+        : DummyData.books.where((p) => p.name.startsWith(query)).toList();
+
+    return ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+            leading: Icon(Icons.picture_in_picture),
+            title: RichText(text: TextSpan(text: suggestionList[index].name))),
+        itemCount: suggestionList.length);
   }
 }
